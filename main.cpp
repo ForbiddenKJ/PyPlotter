e3ec6c7c0a42574180b1c44d73b2470e84f6bc77
ForbiddenKJ <jamesbutcher167@gmail.com>
2021-04-20 22:45:11 +0100

    Added Graph class and restructed and organised code.

--- main.cpp
+++ main.cpp
@@ -5,59 +5,67 @@
 
 Color BACKGROUND = {28, 28, 28, 0};
 
-class PyPlotter{
+class Graph{
 public:
-  void StartWindow(const char* WINDOWNAME, const int SCREENWIDTH, const int SCREENHEIGHT){
-    InitWindow(SCREENWIDTH, SCREENHEIGHT, WINDOWNAME);
+  Vector2 position;
+  std::vector<Vector2> points;
+  Vector2 graphDimention;
+
+  void DrawGraph(){
+    CalculatePoints(-10, 10);
+    PlotPoints();
   }
 
-  std::vector<Vector2> CalculatePoints(int min, int max){
-    std::vector<Vector2> points;
-    float y;
-    float x;
-    for (float i = min; i < max; i++){
-      x = i;
-      y = -pow(x,2);
+private:
+  void PlotPoints(){
+
+    for (int i = 0; i < points.size()-1; i++){
+      DrawLineEx(points[i], points[i+1], 4.0f, RAYWHITE);
+    }
+
+  }
+
+  void CalculatePoints(int minX, int maxX){
+    points.clear();
+
+    for (int i = minX; i < maxX; i++){
+      float x = i;
+      float y = -pow(2,x); // TODO: Customise Equation
 
       x += 40;
       y += 25;
 
       points.push_back((Vector2){x*10, y*10});
     }
-
-    return points;
   }
+};
 
-  // NOTE: Must be called after BeginDrawing(); and Before EndDrawing();
-  void PlotPoints(std::vector<Vector2> points){
-    for (int i = 0; i < points.size()-1; i++){
-      DrawLineEx(points[i], points[i+1], 3.0f, RAYWHITE);
-    }
+class PyPlotter{
+public:
+
+  void StartWindow(const char* WINDOWNAME, const int SCREENWIDTH, const int SCREENHEIGHT){
+    InitWindow(SCREENWIDTH, SCREENHEIGHT, WINDOWNAME);
+    SetTargetFPS(60);
   }
 
 };
 
 int main(){
-  InitWindow(800, 500, "Hello World");
-
-  SetTargetFPS(60);
-
   PyPlotter plotter;
-  std::vector<Vector2> points = plotter.CalculatePoints(-10, 10);
 
-  // Camera2D camera = { 0 };
-  // camera.target = (Vector2){0.0f, 0.0f};
+  plotter.StartWindow("Graph: x^2", 800, 500);
+
+  Graph basicGraph;
 
   while (!WindowShouldClose()){
-    // if (IsKeyDown(KEY_RIGHT)) camera.target.y -= 20;
-    // std::cout << camera.target.y << std::endl;
     BeginDrawing();
-    ClearBackground(BACKGROUND);
-    // BeginMode2D(camera);
 
-    // EndMode2D();
-    plotter.PlotPoints(points);
+    ClearBackground(BACKGROUND);
+
+    basicGraph.DrawGraph();
+
     EndDrawing();
+
   }
 
   CloseWindow();
