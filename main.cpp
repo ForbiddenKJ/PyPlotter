4e5ece1c68eb20afc7a0718c84ac3135da7d6e2e
KJ002 <jamesbutcher167@gmail.com>
2021-04-23 22:24:36 +0100

    Rework of Graph class inheriting from GraphBox

--- main.cpp
+++ main.cpp
@@ -7,80 +7,66 @@
 
 Color BACKGROUND = {28, 28, 28, 0};
 
-class Graph{
-public:
-  Vector2 position = {0.0f, 0.0f};
-  std::vector<Vector2> points;
-
-  Vector2 origin = {0.0f, 0.0f};
-
-  void DrawGraph(){
-    CalculatePoints(-5, 5);
-    PlotPoints();
-    DrawGraphBox();
-
-  }
-
+class GraphBox{
 private:
-  Vector2 graphDimention = {0.0f, 0.0f};
-  float lowestY = 0.0f;
-  float highestY = 0.0f;
+  Vector2 graphBoxPosition;
+  Vector2 graphBoxSize;
 
-  void PlotPoints(){
-    BeginDrawing();
-    for (int i = 0; i < (int)points.size()-1; i++){
-      DrawLineEx(points[i], points[i+1], 2.0f, BLUE);
-    }
-    EndDrawing();
-
+  void CalculateVertices(){
+    graphVertices[0] = (Vector2){graphBoxPosition.x, graphBoxPosition.y};
+    graphVertices[1] = (Vector2){graphBoxPosition.x+graphBoxSize.x, graphBoxPosition.y};
+    graphVertices[2] = (Vector2){graphBoxPosition.x+graphBoxSize.x, graphBoxPosition.y-graphBoxSize.y};
+    graphVertices[3] = (Vector2){graphBoxPosition.x, graphBoxPosition.y-graphBoxSize.y};
   }
 
-  void CalculatePoints(int minX, int maxX, int step = 1, int yCap = 0){
-    points.clear();
+public:
+  Vector2 graphVertices[4];
 
-    bool boundsDefined = false;
-
-    for (int i = minX; i < maxX+1; i+=step){
-      float x = i;
-      float y = -(pow(x,2)+16*x+2); // TODO: Customise Equation
-
-      Vector2 pointPosition = {(x*10)+position.x, (y*10)+position.y};
-
-      if (!boundsDefined){
-        highestY = pointPosition.y;
-        lowestY = pointPosition.y;
-
-        boundsDefined = true;
-      }
-
-      if (pointPosition.y < highestY) highestY = pointPosition.y;
-      if (pointPosition.y > lowestY) lowestY = pointPosition.y;
-
-      if (!(yCap && pointPosition.y < yCap)){
-        points.push_back(pointPosition);
-      }
-
-    }
-
-    graphDimention.x = points.back().x - points.front().x;
-    graphDimention.y = highestY - lowestY;
-
+  void SetPosition(int x, int y){
+    graphBoxPosition = (Vector2){(float)x, (float)y};
   }
 
-  void DrawGraphBox(){
-    BeginDrawing();
+  void SetSize(int x, int y){
+    graphBoxSize = (Vector2){(float)x, (float)y};
+  }
 
-    Vector2 graphPoint[4] = {(Vector2){points.front().x, lowestY},
-                            (Vector2){points.front().x+graphDimention.x, lowestY},
-                            (Vector2){points.front().x+graphDimention.x, highestY},
-                            (Vector2){points.front().x, highestY}};
+  Vector2 GetPosition(){
+    return graphBoxPosition;
+  }
+
+  Vector2 GetSize(){
+    return graphBoxSize;
+  }
+
+  void Draw(){
+    CalculateVertices();
+    BeginDrawing();
 
     for (int i = 0; i < 4; i++){
-      DrawLineEx(graphPoint[i%4], graphPoint[(i+1)%4], 3.0f, RAYWHITE);
+      DrawLineEx(graphVertices[i%4], graphVertices[(i+1)%4], 3.0f, RAYWHITE);
     }
 
     EndDrawing();
   }
+
+};
+
+class Graph : public GraphBox{
+private:
+  Vector2 center;
+  Vector2 origin;
+
+public:
+
+  void SetOrigin(Vector2 pos){
+    center = {GetPosition().x+(GetSize().x/2),
+             GetPosition().y-(GetSize().y/2)};
+
+    origin = (Vector2){center.x + pos.x,
+             center.y - pos.y};
+
+  }
+
 };
 
 class PyPlotter{
@@ -111,11 +97,14 @@ int main(){
 
   plotter.StartWindow("Graph: 2^x", 800, 500);
 
-  Graph basicGraph;
+  Graph basicGraphBox;
+  basicGraphBox.SetPosition(400, 250);
+  basicGraphBox.SetSize(100, 100);
 
-  basicGraph.position = {400, 250};
-  basicGraph.DrawGraph();
+  basicGraphBox.Draw();
 
+  basicGraphBox.SetOrigin((Vector2){-10.0f, 0.0f});
+  
   plotter.KeepWindowAlive();
 
   return 0;
