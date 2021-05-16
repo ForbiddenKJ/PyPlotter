ba58f08f4021b3ca59388b79a3c24daa73735557
KJ002 <jamesbutcher167@gmail.com>
2021-05-16 20:12:26 +0100

    Updated example in compilance to src

--- example.py
+++ example.py
@@ -3,11 +3,7 @@ from PyPlotter import Plotter, Graph
 plotter = Plotter("Graph", 800, 500)
 graph = Graph(400, 250, 100, 200)
 
-graph.draw()
-graph.set_origin(0, 0)
-graph.draw_axis()
-graph.draw_number()
-graph.calculate(-100, 100, "x^2")
-graph.draw_points()
+graph.draw(0, 0)
+graph.plot(-100, 100, "x^2")
 
 plotter.main_loop()
