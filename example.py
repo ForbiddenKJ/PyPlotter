8ce3e70d2320b9d3ea71f45e3821be7e0230942e
KJ002 <jamesbutcher167@gmail.com>
2021-05-18 19:45:44 +0100

    Fixed to support Line class

--- example.py
+++ example.py
@@ -1,9 +1,12 @@
-from PyPlotter import Plotter, Graph
+from PyPlotter import Plotter, Graph, Line
 
 plotter = Plotter("Graph", 800, 500)
 graph = Graph(400, 250, 100, 200)
+line = Line()
+
+line.calculate(-100, 100, "x^2")
 
 graph.draw(0, 0)
-graph.plot(-100, 100, "x^2")
+graph.draw_line(line)
 
 plotter.main_loop()
